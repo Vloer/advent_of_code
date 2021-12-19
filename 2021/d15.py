a5bf991049c3da45e3151a61a6ae800301055bdc
Vloer <borisruwe@hotmail.com>
2021-12-19 17:21:55 +0100

    d15a

--- 2021/d15.py
+++ 2021/d15.py
@@ -34,11 +34,11 @@ def get_adj_indices(mat: np.ndarray, node: tuple(int)) -> list(tuple(int)):
     col = node[1]
     if row > 0:
         adj.append((row-1, col))
-    if row+1 < mat.shape[0]-1:
+    if row+1 <= mat.shape[0]-1:
         adj.append((row+1, col))
     if col > 0:
         adj.append((row, col-1))
-    if col+1 < mat.shape[1]-1:
+    if col+1 <= mat.shape[1]-1:
         adj.append((row, col+1))
     return adj
 
@@ -57,17 +57,18 @@ class Node:
         self.dist_start = 0
         self.dist_goal = 0
         self.dist_tot = 0
-    
+
     def __eq__(self, other: Node):
         return self.position == other.position
-    
+
     def __lt__(self, other: Node):
         return self.dist_tot < other.dist_tot
 
     def __repr__(self):
         return (f'({self.position}, {self.dist_tot})')
 
-def astar_search(mat: np.ndarray, start: tuple(int), end: tuple(int)):
+
+def astar_search(mat: np.ndarray, start: tuple(int), end: tuple(int)) -> list(tuple) | int:
     open = []
     closed = []
     start_node = Node(start, None)
@@ -77,14 +78,17 @@ def astar_search(mat: np.ndarray, start: tuple(int), end: tuple(int)):
     while len(open) > 0:
         open.sort()
         current_node: Node = open.pop(0)
+        if current_node.position == (8,8):
+            print('k')
         closed.append(current_node)
         if current_node == end_node:
+            total_cost = current_node.dist_tot
             path = []
             while current_node != start_node:
                 path.append(current_node.position)
                 current_node = current_node.parent
-            return path[::-1]
-        
+            return path[::-1], total_cost
+
         adjacent = get_adj_indices(mat, current_node.position)
 
         for next in adjacent:
@@ -94,116 +98,30 @@ def astar_search(mat: np.ndarray, start: tuple(int), end: tuple(int)):
                 continue
 
             # Generate heuristic
-            adj.dist_start = abs(adj.position[0] - start_node.position[0]) + abs(adj.position[1] - start_node.position[1])
-            adj.dist_goal = abs(adj.position[0] - adj.position[0]) + abs(adj.position[1] - adj.position[1])
+            adj.dist_start = current_node.dist_tot
+            adj.dist_goal = value
             adj.dist_tot = adj.dist_start + adj.dist_goal
 
-class Graph:
-    # example of adjacency list (or rather map)
-    # adjacency_list = {
-    # 'A': [('B', 1), ('C', 3), ('D', 7)],
-    # 'B': [('D', 5)],
-    # 'C': [('D', 12)]
-    # }
+            # Check if neighbor is in open list and if it has a lower total value
+            if add_to_open(open, adj):
+                open.append(adj)
 
-    def __init__(self, adjacency_list):
-        self.adjacency_list = adjacency_list
+    # Return None, no path is found
+    return None, None
 
-    def get_neighbors(self, v):
-        return self.adjacency_list[v]
 
-    # heuristic function with equal values for all nodes
-    def h(self, n):
-        H = {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1
-        }
-
-        return H[n]
-
-    def a_star_algorithm(self, start_node, stop_node):
-        # open_list is a list of nodes which have been visited, but who's neighbors
-        # haven't all been inspected, starts off with the start node
-        # closed_list is a list of nodes which have been visited
-        # and who's neighbors have been inspected
-        open_list = set([start_node])
-        closed_list = set([])
-
-        # g contains current distances from start_node to all other nodes
-        # the default value (if it's not found in the map) is +infinity
-        g = {}
-
-        g[start_node] = 0
-
-        # parents contains an adjacency map of all nodes
-        parents = {}
-        parents[start_node] = start_node
-
-        while len(open_list) > 0:
-            n = None
-
-            # find a node with the lowest value of f() - evaluation function
-            for v in open_list:
-                if n is None or g[v] + self.h(v) < g[n] + self.h(n):
-                    n = v
-
-            if n is None:
-                print('Path does not exist!')
-                return None
-
-            # if the current node is the stop_node
-            # then we begin reconstructin the path from it to the start_node
-            if n == stop_node:
-                reconst_path = []
-
-                while parents[n] != n:
-                    reconst_path.append(n)
-                    n = parents[n]
-
-                reconst_path.append(start_node)
-
-                reconst_path.reverse()
-
-                print('Path found: {}'.format(reconst_path))
-                return reconst_path
-
-            # for all neighbors of the current node do
-            for (m, weight) in self.get_neighbors(n):
-                # if the current node isn't in both open_list and closed_list
-                # add it to open_list and note n as it's parent
-                if m not in open_list and m not in closed_list:
-                    open_list.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + weight
-
-                # otherwise, check if it's quicker to first visit n, then m
-                # and if it is, update parent data and g data
-                # and if the node was in the closed_list, move it to open_list
-                else:
-                    if g[m] > g[n] + weight:
-                        g[m] = g[n] + weight
-                        parents[m] = n
-
-                        if m in closed_list:
-                            closed_list.remove(m)
-                            open_list.add(m)
-
-            # remove n from the open_list, and add it to closed_list
-            # because all of his neighbors were inspected
-            open_list.remove(n)
-            closed_list.add(n)
-
-        print('Path does not exist!')
-        return None
+def add_to_open(open: list[Node], neighbor: Node):
+    for node in open:
+        if (neighbor == node and neighbor.dist_tot >= node.dist_tot):
+            return False
+    return True
 
 
 def solve1(data: np.ndarray, result: int = 0) -> int:
     start = (0, 0)
     end = (data.shape[0] - 1, data.shape[1] - 1)
-
-    return result
+    path, cost = astar_search(data, start, end)
+    return cost
 
 
 def solve2(data: list[int], result: int = 0) -> int:
@@ -211,4 +129,4 @@ def solve2(data: list[int], result: int = 0) -> int:
 
 
 print(f"Answer 1: {solve1(test)}")
-print(f"Answer 2: {solve2(inp)}")
+print(f"Answer 2: {solve2(test)}")
