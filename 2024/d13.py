32c3eeb29ff0bf72b721ecb7afccf18722f0e501
Vloer <borisruwe@hotmail.com>
2024-12-09 21:27:30 +0100

    update template

--- 2024/d13.py
+++ 2024/d13.py
@@ -1,6 +1,22 @@
 from __future__ import annotations
 from pathlib import Path
-from time import perf_counter
+import time
+from typing import Callable, Any
+import functools
+
+
+def timer(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Function '{func.__name__}' took {execution_time:.4f} seconds to execute")
+        return result
+
+    return wrapper
+
 
 input_file = Path(__file__).parent / "inputs" / f'{Path(__file__).stem}.txt'
 
@@ -11,15 +27,14 @@ def parse_input(txt_file: str = input_file) -> list[str]:
 
 
 inp = parse_input()
+test = """""".split('\n')
 
 
 def solve(data: list[str], result: int = 0, part1=True) -> int:
     return result
 
 
-timing_1 = perf_counter()
-answer_1 = solve(inp)
-print(f"Answer 1 took {perf_counter() - timing_1}: {answer_1}")
-timing_2 = perf_counter()
-answer_2 = solve(inp, part1=False)
-print(f"Answer 2 took {perf_counter() - timing_2}: {answer_2}")
+answer_1 = solve(test)
+print(f"Answer 1: {answer_1}")
+answer_2 = solve(test, part1=False)
+print(f"Answer 2: {answer_2}")
