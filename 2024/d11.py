49b3eca2668d60f97697b661abb4ed15bf6baf03
Vloer <borisruwe@hotmail.com>
2024-12-17 11:10:45 +0100

    d11

--- 2024/d11.py
+++ 2024/d11.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 from pathlib import Path
 import time
-from typing import Callable, Any
+from typing import Callable, Any, Optional
 import functools
+from copy import deepcopy
+import math
+from collections import deque
 
 
 def timer(func: Callable) -> Callable:
@@ -18,27 +21,43 @@ def timer(func: Callable) -> Callable:
     return wrapper
 
 
-input_file = Path(__file__).parent / "inputs" / f'{Path(__file__).stem}.txt'
+input_file = Path(__file__).parent / "inputs" / f"{Path(__file__).stem}.txt"
 
 
-def parse_input(txt_file: str = input_file) -> list[str]:
-    with open(txt_file, 'r') as f:
-        return f.read().split("\n")
+def parse_input(txt_file: str = input_file) -> str:
+    with open(txt_file, "r") as f:
+        return f.read()
 
 
 inp = parse_input()
-test = """""".split('\n')
+test = """125 17"""
+
+
+@functools.lru_cache(maxsize=None)
+def calculate(n: int, remaining_blinks: int, total: Optional[int] = None) -> int:
+    if total is None:
+        total = 0
+    if remaining_blinks == 0:
+        total += 1
+        return total
+    if n == 0:
+        return calculate(1, remaining_blinks - 1, total)
+    length = int(math.log10(n)) + 1
+    if length % 2 == 0:
+        left = n // (1 * 10 ** (length // 2))
+        right = n % (1 * 10 ** (length // 2))
+        return calculate(left, remaining_blinks - 1, total) + calculate(right, remaining_blinks - 1, total)
+    return calculate(n * 2024, remaining_blinks - 1, total)
 
 
 @timer
-def solve1(data: list[str], result: int = 0) -> int:
-    return result
+def solve(data: str, blinks: int) -> int:
+    stones = [int(n) for n in data.strip().split()]
+    final = 0
+    for n in stones:
+        final += calculate(n, blinks)
+    return final
 
-@timer
-def solve2(data: list[str], result: int = 0) -> int:
-    return result
 
-
-
-answer_1 = solve1(inp)
-answer_2 = solve2(inp)
+answer_1 = solve(inp, 25)
+answer_2 = solve(inp, 75)
